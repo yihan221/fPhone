d494a2509f26c926494db07bcafa6ab00c07c9eb
blade30719 <62998425+blade30719@users.noreply.github.com>
2021-05-16 00:02:18 +0800

    '加三行空白哈哈'

---
+++ finalMproject/app/src/main/java/tw/edu/pu/s1071551/finalproject/MainActivity.java
@@ -0,0 +1,19 @@
+package tw.edu.pu.s1071551.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+    
+}
